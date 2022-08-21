9559f2c06b9834a073994d6927c664d044e7dab8
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-08-21 15:19:51 +0300

    feat: настроить сервер

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from "./file/file.module";
+import { TrackModule } from "./track/track.module";
+import * as path from 'path'
+import { ServeStaticModule } from '@nestjs/serve-static'
+
+@Module({
+    imports: [
+        ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+        MongooseModule.forRoot('mongodb+srv://music-user-admin:music-password@cluster0.vig2olo.mongodb.net/?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule,
+    ],
+})
+export class AppModule { }
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+
+const start = async () => {
+    try {
+        const PORT = process.env.PORT || 5000;
+        const app = await NestFactory.create(AppModule);
+
+        app.enableCors()
+
+        await app.listen(PORT, () => console.log(`Сервер запустился на ${PORT} порту`))
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
